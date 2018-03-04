27f951ee192a41e346c6daf6701f21559c41a218
root <root@node1.c.operating-edge-190214.internal>
2018-03-04 16:25:27 +0000

    Welcome

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to the java first program");
+
+	}
+
+}
